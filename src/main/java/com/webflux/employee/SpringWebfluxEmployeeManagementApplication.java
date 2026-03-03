676a9d7a9fe64af45c070cf2cd0533c1b11af564
07Rochak <rochakshrivastav02@gmail.com>
2026-03-03 23:53:50 +0530

    Initial commit

---
+++ src/main/java/com/webflux/employee/SpringWebfluxEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webflux/employee/SpringWebfluxEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxEmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
